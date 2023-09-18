7182e3614a007540fede7292c995cc5267f1d173
bastelbude-01 <messerschmidt.markus@web.de>
2023-09-18 20:51:31 +0200

    added work

--- scout2/launch/launch_sim.launch.py
+++ scout2/launch/launch_sim.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='homer' 
+    package_name='scout2' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -42,14 +42,14 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])#,
+                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'homer'],
+                                   '-entity', 'scout2'],
                         output='screen')
 
 
@@ -87,10 +87,10 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        camera,
+        #camera,
         #joystick,
         gazebo,
-        spawn_entity,
+        spawn_entity#,
         #diff_drive_spawner,
         #joint_broad_spawner
     ])
